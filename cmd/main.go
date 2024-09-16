9231312c805e00dc7a598943e9c60149bcc45ed4
Nolan Bradshaw <nolanbradshaw22@gmail.com>
2024-09-15 21:19:34 -0400

    adding commit command

---
+++ cmd/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"git-pilot/internal/runners"
+	"log"
+
+	"github.com/spf13/cobra"
+)
+
+func main() {
+	var rootCmd = &cobra.Command{
+		Use:   "git-pilot",
+		Short: "GitPilot is a command line tool for managing git repositories.",
+	}
+
+	git_runner := &runners.GitRunner{}
+	var message string
+	var createCommitCmd = &cobra.Command{
+		Use:   "create-commit",
+		Short: "Generate a commit and push it to the remote repository.",
+		Run: func(cmd *cobra.Command, args []string) {
+			// TODO: Handle errors more gracefully.
+			err := git_runner.StageAndCommit(message)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			err = git_runner.Push()
+			if err != nil {
+				log.Fatal(err)
+			}
+		},
+	}
+
+	createCommitCmd.Flags().StringVarP(&message, "message", "m", "", "Commit Message.")
+
+	rootCmd.AddCommand(createCommitCmd)
+	rootCmd.Execute()
+}
